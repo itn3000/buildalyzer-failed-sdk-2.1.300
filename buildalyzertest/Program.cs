31f7f7b8dd7b1d407be16ef11ad57b9c1eed7f50
itn3000 <itn3000@gmail.com>
2018-06-08 10:21:59 +0900

    first commit

---
+++ buildalyzertest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Buildalyzer;
+using Microsoft.Build;
+using Microsoft.Build.Framework;
+
+namespace buildalyzertest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var manageropts = new AnalyzerManagerOptions();
+                manageropts.LogWriter = Console.Out;
+                manageropts.LoggerVerbosity = LoggerVerbosity.Normal;
+                var manager = new AnalyzerManager(manageropts);
+                var analyzer = manager.GetProject(args[0]);
+                var project = analyzer.Compile();
+                if (project != null)
+                {
+                    foreach (var x in project.Items)
+                    {
+                        var meta = x.Metadata.Select(y => $"{y.Name}={y.EvaluatedValue}");
+                        Console.WriteLine($"{x.ItemType},{string.Join("|", meta)}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"project instance is null");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e}");
+            }
+        }
+    }
+}
